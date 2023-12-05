2f70a0431ad029d96d2a069877626d0e4a8b6fe4
kimonos <youhappy421@gmail.com>
2023-12-05 16:37:17 +0800

    version_3

--- src/main/java/com/example/security_jwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/security_jwt/config/JwtAuthenticationFilter.java
@@ -19,24 +19,25 @@ import java.io.IOException;
 @Component //將其視為spring boot泛用組件
 @RequiredArgsConstructor //生成一個包含 “特定參數” 的 constructor，特定參數指的是那些有加上 final 修飾詞的變量們
 //繼承OncePerRequestFilter，是確保每一次的請求只會經過一次的filter，不會重複執行
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter { //確保單一請求的執行緒只會被呼叫一次
+
 
     private final JwtService jwtService;//引入商業邏輯
     private final UserDetailsService userDetailsService; //對於Security中的使用者的商業邏輯
-
     @Override
-    protected void doFilterInternal(//確保單一請求的執行緒只會被呼叫一次
+    protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain //FilterChain為各個Filter的集合，其中會包含各個不同的Filter
             //@NonNull表示對應值不能為空值
     ) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authentication");//設定一個字串變數authHeader來儲存"Authenication"標頭的值
-        final String jwt; //設定一個字串變數jwt
-        final String username; //設定一個字串變數username
-        if(authHeader == null || !authHeader.startsWith("Bearer ")){ //當authHeader是空值或者authHeader不是開始於"Bearer "時
+
+        final String authHeader = request.getHeader("Authorization");//設定一個字串變數authHeader來儲存"Authenication"標頭的值
+        final String jwt;
+        final String username;
+        if (authHeader == null|| !authHeader.startsWith("Bearer ")) {//當authHeader是空值或者authHeader不是開始於"Bearer "時
             filterChain.doFilter(request,response);//直接進行下一個Filter
-            return; //終止當前的filter
+            return;//終止當前的filter
         }
         jwt = authHeader.substring(7);//將jwt值設為"Bearer "後方的token
         username = jwtService.extractUsername(jwt);
@@ -54,5 +55,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);//在SecurityContextHolder設置使用者相關訊息
             }
         }
+        filterChain.doFilter(request,response);
     }
 }
